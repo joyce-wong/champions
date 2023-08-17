693ba40b99c08576a189f99941bb9fa6a0967ae3
Joyce-Wong <joycekwo12@gmail.com>
2023-08-17 17:03:08 -0400

    add items to database in realtime

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // javascript
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 
 const appSettings = {
@@ -11,14 +11,40 @@ const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const recommendationsInDB = ref(database, "recommendations")
 
+onValue(recommendationsInDB, function(snapshot){
+    let itemsArray = Object.values(snapshot.val())
+    let recommendationsArray = itemsArray
+    
+    clearInputFieldEl()
 
+    recommendationsArray.map(recommendation => {
+        addToList(recommendation)
+    })
+})
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const recommendationsListEl = document.getElementById("recommendations-list")
 
 addButtonEl.addEventListener("click", function(){
     let inputValue = inputFieldEl.value
 
     push(recommendationsInDB, inputValue)
-    console.log(inputValue)
+
+    clearInputFieldEl()
+    addToList(inputValue)
+
 })
+
+onValue(recommendationsInDB, function(snapshot){
+    console.log(snapshot.val())
+})
+
+function clearInputFieldEl(){
+    inputFieldEl.value = ""
+}
+
+function addToList(itemValue){
+    recommendationsListEl.innerHTML += `<li>${itemValue}</li>`
+    
+}
